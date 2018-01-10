eddf65d2290a3f267c64fdf977b69ca9558a3308
Amit Joshi <amitjoshiave@gmail.com>
2018-01-10 20:02:45 +0100

    updates

--- src/main/java/com/candidate/demo/service/CandidateService.java
+++ src/main/java/com/candidate/demo/service/CandidateService.java
@@ -14,17 +14,30 @@ public class CandidateService {
     @Autowired
     private CandidateRepository candidateRepository;
 
-
+    /**
+     * Saves a candidate object with a new id
+     * @param candidate object
+     * @return saved object
+     */
     public Candidate save(Candidate candidate){
         return candidateRepository.save(candidate);
     }
 
 
+    /**
+     * finds all candidates
+     * @return list of candidates
+     */
     public List<Candidate> findAll(){
         return candidateRepository.findAll();
     }
 
 
+    /**
+     * finds a candidate by rank
+     * @param rank to find a candidate with
+     * @return candidates with given rank
+     */
     public List<Candidate> findByRank(int rank){
         return candidateRepository.findByRank(rank);
     }
